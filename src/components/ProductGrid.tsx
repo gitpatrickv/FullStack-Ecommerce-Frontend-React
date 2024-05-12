3f7b0893c158b54c0f05dc5c839f0f26fc6ca954
rektgit2024 <myemailispatrick@gmail.com>
2024-05-12 20:58:52 +0800

    refactor useProducts hook

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,18 +1,21 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Spinner } from "@chakra-ui/react";
 import useProducts from "../hooks/useProducts";
 import ProductCard from "./ProductCard";
 import ProductCardContainer from "./ProductCardContainer";
 
 const ProductGrid = () => {
-  const products = useProducts();
+  // const products = useProducts();
+  const { data, isLoading, error } = useProducts();
+  if (isLoading) return <Spinner />;
 
+  if (error || !data) throw error;
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 3, lg: 3, xl: 5 }}
       spacing={6}
       padding="10px"
     >
-      {products.map((prod) => (
+      {data?.data.map((prod) => (
         <ProductCardContainer key={prod.productId}>
           <ProductCard product={prod} />
         </ProductCardContainer>
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,29 +1,12 @@
 import { useEffect, useState } from "react";
 import Product from "../entities/Product";
 import { axiosInstance } from "../services/api-client";
+import { useQuery } from "@tanstack/react-query";
 
 const apiClient = axiosInstance;
 
-const useProducts = () => {
-    const [products, setProducts] = useState<Product[]>([]); 
-  
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-   const response = await apiClient.get<Product[]>("/product");
-        setProducts(response.data);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
-        
-        fetchData();
-    }, []);
-
-  return (
-
-    products
-  )
-}
-
+const useProducts = () => useQuery ({
+  queryKey: ['product'],
+  queryFn: () => apiClient.get<Product[]>('product'),
+});
 export default useProducts
