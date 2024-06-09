fb4a5e5ec9f6432783e5a31451805942726167d2
rektgit2024 <myemailispatrick@gmail.com>
2024-06-09 22:33:27 +0800

    add refetch product  on ProductCard component to sync quantity after placing order

--- src/hooks/usePlaceOrder.ts
+++ src/hooks/usePlaceOrder.ts
@@ -20,7 +20,7 @@ const usePlaceOrder = () => {
         },
         {
         onSuccess: () => {
-            queryClient.invalidateQueries(['orders', 'cart' , 'cartTotal'])
+            queryClient.invalidateQueries(['orders', 'cart' , 'cartTotal', 'product'])
             
             toast({
                 position: "top",
