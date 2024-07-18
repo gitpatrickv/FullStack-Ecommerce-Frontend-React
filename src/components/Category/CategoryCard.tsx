e5dabc97403b7a5dd1c4619c415fac872ddb905a
rektgit2024 <myemailispatrick@gmail.com>
2024-07-18 13:24:09 +0800

    implemented carousel for categories in home page

--- src/components/Category/CategoryCard.tsx
+++ src/components/Category/CategoryCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Card, CardBody, Text } from "@chakra-ui/react";
+import { Avatar, Box, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import Category from "../../entities/Category";
 
@@ -14,32 +14,36 @@ const CategoryCard = ({ category }: Props) => {
   };
 
   return (
-    <Card
+    <Box
       maxW="150px"
       height="150px"
       cursor="pointer"
+      border="1px solid"
       onClick={handleNavigateClick}
+      borderColor="gray.500"
+      _hover={{
+        color: "orange.500",
+      }}
     >
-      <CardBody>
-        <Box
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Avatar
-            src={
-              category.categoryPhotoUrl ||
-              "https://st4.depositphotos.com/38069286/41918/v/450/depositphotos_419187960-stock-illustration-shopping-bag-icon-purchase-symbol.jpg"
-            }
-            size="lg"
-          />
-          <Text fontSize="small" mt="10px" textAlign="center">
-            {category.categoryName}
-          </Text>
-        </Box>
-      </CardBody>
-    </Card>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        mt="20px"
+      >
+        <Avatar
+          src={
+            category.categoryPhotoUrl ||
+            "https://st4.depositphotos.com/38069286/41918/v/450/depositphotos_419187960-stock-illustration-shopping-bag-icon-purchase-symbol.jpg"
+          }
+          size="lg"
+        />
+        <Text fontSize="14px" mt="10px" textAlign="center">
+          {category.categoryName}
+        </Text>
+      </Box>
+    </Box>
   );
 };
 
