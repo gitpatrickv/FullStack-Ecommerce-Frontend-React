14d91d95d24ef21ef403017de0f0c6f897b1976b
rektgit2024 <myemailispatrick@gmail.com>
2024-08-24 14:35:37 +0800

    refactored useCreateChat hook

--- src/hooks/user/useCreateChat.ts
+++ src/hooks/user/useCreateChat.ts
@@ -1,8 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { axiosInstance } from "../../services/api-client";
 import { useAuthQueryStore } from "../../store/auth-store";
+import { useChatStore } from "../../store/chat-store";
 
 const apiClient = axiosInstance;
+
+interface ChatIdResponse {
+    chatId: number;
+  }
 
 interface Props {
     recipient: string;
@@ -12,20 +17,24 @@ const useCreateChat = () => {
     const queryClient = useQueryClient();
     const { authStore } = useAuthQueryStore();
     const jwtToken = authStore.jwtToken;
+    const { setChatId } = useChatStore();
 
     return useMutation(
         async({recipient} : Props) => {
-            await apiClient.post(`/chat/${recipient}`, {},
+            const response = await apiClient.post<ChatIdResponse>(`/chat/${recipient}`, {},
                 {
                     headers: {
                         Authorization: `Bearer ${jwtToken}`,
                     }
                 }
-            )            
+            )    
+            return response.data;        
         },
         {
-            onSuccess: () => {
+            onSuccess: (data) => {
               queryClient.invalidateQueries(['chatList']);
+              const storeChatId = data.chatId;
+              setChatId(storeChatId);
             }
           },
     )
